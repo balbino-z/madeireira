5f9be2d0ca00a1ef9765d57ab2734bcddc601e61
balbino-z <balbinobarretoneide@gmail.com>
2025-02-16 00:16:36 -0300

    Preparando para deploy no Render

--- madeireira/wsgi.py
+++ madeireira/wsgi.py
@@ -14,3 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'madeireira.settings')
 
 application = get_wsgi_application()
+
+import django
+django.setup()
+from django.contrib.auth.models import User
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "vinicius", "barreto")
+    print("Superusuário criado!")
